a8d92a2833c6b45889c0fd9d6105750ac9b9ec59
Xiaoyang Ma <mxy2000ast@126.com>
2019-01-18 10:23:21 +0800

    ex1 solution

--- machine-learning-ex1/ex1/ex1_multi.m
+++ machine-learning-ex1/ex1/ex1_multi.m
@@ -82,7 +82,7 @@ X = [ones(m, 1) X];
 fprintf('Running gradient descent ...\n');
 
 % Choose some alpha value
-alpha = 0.01;
+alpha = 1;
 num_iters = 400;
 
 % Init Theta and Run Gradient Descent 
@@ -91,7 +91,8 @@ theta = zeros(3, 1);
 
 % Plot the convergence graph
 figure;
-plot(1:numel(J_history), J_history, '-b', 'LineWidth', 2);
+%plot(1:numel(J_history), J_history, '-b', 'LineWidth', 2);
+plot(1:20, J_history(1:20), '-b', 'LineWidth', 2);
 xlabel('Number of iterations');
 ylabel('Cost J');
 
@@ -104,7 +105,7 @@ fprintf('\n');
 % ====================== YOUR CODE HERE ======================
 % Recall that the first column of X is all-ones. Thus, it does
 % not need to be normalized.
-price = 0; % You should change this
+price = X * theta; % You should change this
 
 
 % ============================================================
@@ -149,7 +150,7 @@ fprintf('\n');
 
 % Estimate the price of a 1650 sq-ft, 3 br house
 % ====================== YOUR CODE HERE ======================
-price = 0; % You should change this
+price = X * theta; % You should change this
 
 
 % ============================================================
